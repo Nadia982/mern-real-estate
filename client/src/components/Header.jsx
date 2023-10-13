38262342399658297e6bbae2f1faedaa4a59eb48
Natalie Gillam <ngillam@hotmail.co.uk>
2023-10-13 23:55:19 +0100

    updates to Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <header>
+      <h1 className = "font-bold text-sm sm:text-xl">
+        <span className="text-slate-500">Nadia</span>
+        <span className="text-slate-700">Real Estate</span>
+      </h1>
+    </header>
+  );
 }
