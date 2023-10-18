ff0c24c28762d351d314e199868dad65e4b7da5f
Natalie Gillam <ngillam@hotmail.co.uk>
2023-10-18 22:23:11 +0100

    created sign-up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();    
 
 mongoose.connect(process.env.MONGO).then(() =>{
@@ -14,6 +15,8 @@ const router = express.Router();
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000,()=>{
     console.log('Server is running on port 4000')
 })
@@ -22,4 +25,5 @@ app.get('/test', (req, res) => {
     res.json({message: 'Hello, universe'});
 })
 
-app.use("/api/user", userRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
