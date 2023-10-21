ce1b0fd3f5060805f530979f191bbb50acd53aa8
Natalie Gillam <ngillam@hotmail.co.uk>
2023-10-21 23:00:50 +0100

    created sign in API route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
             <span className="text-slate-500">Nadia</span>
-            <span className="text-slate-700">RealEstate</span>
+            <span className="text-slate-700">Homes</span>
           </h1>
         </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
